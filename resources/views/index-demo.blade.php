28cc45dfbf68811091fb9e2895cffbea7d2fb49c
Nikko Dutra Bouck <nikko.novitas@gmail.com>
2019-10-19 19:58:51 -0700

    Update1

--- resources/views/index-demo.blade.php
+++ resources/views/index-demo.blade.php
@@ -2,22 +2,20 @@
 
 @section('content')
 
-					
+	@include('story-components.nav')
 
-					@include('story-components.nav')
+	@include('story-components.main-menu')
 
-				  @include('story-components.main-menu')
+	@include('story-components.about')
 
-					@include('story-components.about')
+	@include('story-components.how-nextbin-works')
 
-					@include('story-components.how-nextbin-works')
+	@include('story-components.pricing')
 
-					@include('story-components.pricing')
+	@include('story-components.farms')
 
-					@include('story-components.farms')
+	@include('story-components.customers')
 
-					@include('story-components.customers')
-
-					@include('story-components.footer')
+	@include('story-components.footer')
 
 @endsection
--- resources/views/ui-dev.blade.php
+++ resources/views/ui-dev.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
 
-					@include('story-components.nav')
+	@include('story-components.nav')
 
-				  @include('story-components.main-menu')
+	@include('story-components.main-menu')
 
-					@include('story-components.about')
+	@include('story-components.about')
 
-					@include('story-components.how-nextbin-works')
+	@include('story-components.how-nextbin-works')
 
-					@include('story-components.pricing')
+	@include('story-components.pricing')
 
-					@include('story-components.farms')
+	@include('story-components.farms')
 
-					@include('story-components.customers')
+	@include('story-components.customers')
 
-					<!-- Dev content -->
-					@include('story-components.additional-elements')
+	<!-- Dev content -->
+	@include('story-components.additional-elements')
 
-					@include('story-components.footer')
+	@include('story-components.footer')
 
 @endsection
